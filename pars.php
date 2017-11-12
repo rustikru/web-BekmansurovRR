9fcb7316dde68aaa737b3e4240d54903e17a497c
rustikru <rustam1204@icloud.com>
2017-11-12 17:17:40 +0500

    Добавлен парсер с помощью регулярных выражений

--- pars.php
+++ pars.php
@@ -2,43 +2,7 @@
 	require_once( "block/bd.php" );
 	require_once("lib/phpQuery-onefile.php");
 	require_once("lib/simple_html_dom.php");
-function run_pars_phpquery(){
-	$i = 1;
-	$products = array();
-	$pagesCount=1;
-	$c = 0;
-	do { 
-		$url = "https://biggeek.ru/catalog/cmartfony-xiaomi";
-		//echo $url;
-		$html = file_get_contents($url);
-		phpQuery::newDocument($html);
-		foreach(pq('div.item-wrap') as $product){
-			$products[$c]['img'] = pq($product)->find('a')->attr('href');
-			$products[$c]['imgUrl'] = pq($product)->find('img')->attr('src');
-			$products[$c]['name'] = pq($product)->find('img')->attr('alt');
-			if(empty($products[$c]['price'])) { 
-				$products[$c]['price_del'] = pq($product)->find('del')->text();
-				$products[$c]['price_ins'] = pq($product)->find('ins')->text(); 
-				$products[$c]['price_del'] = trim($products[$c]['price_del']);
-				$products[$c]['price_ins'] = trim($products[$c]['price_ins']);
-			}
-			$c++;
-		}
-		$i++;
-	} while($i<=$pagesCount);
-	//$r = mysql_query ("delete from tovar");
-	foreach ($products as $key => $value) {
-		$name=$products[$key]['name'];
-		$imgUrl=$products[$key]['imgUrl'];
-		$price_del = floatval(preg_replace("/[^-0-9\.]/","",$products[$key]['price_del']));
-		if ($price_del ==''){
-			$price_del=0;
-		}
-		$price_ins= floatval(preg_replace("/[^-0-9\.]/","",$products[$key]['price_ins']));
-		$r = mysql_query ("insert into tovar (name, imgurl, price_del, price_ins) values ('".$name."','".$imgUrl."',".$price_del.",".$price_ins.")");
-	}
-	//print_r($products);
-}
+// парсер с помощью библиотеки simple_html_dom
 function run_pars_dom () {
 	$pagesCount=3;
 	$i = 1;
@@ -86,21 +50,23 @@ function run_pars_dom () {
 	}
 	//print_r($products);
 };
+// парсер с помощью регулярных выражений
 function preg_rep(){
-	$pagesCount=1;
-	$i = 1;
-	$products = array();
-	$c = 0;
-	//do { 
-		#адресс сайта
-		$url = "https://biggeek.ru/catalog/noutbuki-apple?page=1";//.$i;
-		$html = file_get_contents($url);
-		preg_match("/<div.item-wrap.'\/(.*?)<\/a>/", $html, $text);
-		var_dump($text);
-	//	$i++;
-	//} while($i<=$pagesCount);
+	$tovar = array();
+	$html = file_get_contents("https://biggeek.ru/catalog/noutbuki-apple?page=1");
+	preg_match_all("|<div class=\"item-wrap\">.+?<a.+?class=\"img\">.+?<img src=\"(.+?)\".+?>.+?</a>.+?<div class=\"item-info\">.+?<a.+?>(.+?)</a>.+?<div class=\"price\">.+?<ins>(.+?),-</ins>.+?</div>.+?</div>.+?</div>|s", $html, $tovar, PREG_SET_ORDER);
+	//var_dump($tools);
+	
+	foreach ( $tovar as $item ) {
+		$name=$item[2];
+		$imgUrl=$item[1];
+		$price_ins= 0;
+		$price_del= 0;
+		if (isset($item[3])) { 
+			$price_ins= floatval(preg_replace("/[^-0-9\.]/","",$item[3]));
+		} else $price_ins= 0;
+		$r = mysql_query ("insert into tovar (name, imgurl, price_del, price_ins) values ('".$name."','".$imgUrl."',".$price_del.",".$price_ins.")");
+	}
 }
-run_pars_dom();
-//run_pars_phpquery();
 ?>
 
--- send.php
+++ send.php
@@ -1,51 +1,66 @@
 <?php
 	require_once( "block/bd.php" );
-	require_once("pars.php");
+	require_once("pars.php"); // файл с парсером
 	$res='';
-	if ($_GET['action'] =='add'){
-		$pname = '';
-		$pemail='';
-		$ptext='';
-		if (isset($_POST['pname'])) {
-			$pname = $_POST['pname'];
-		} else {$pname = '';}
+// функция обработки строки
+function StrProcess($str)
+{
+      $str= stripslashes($str); // удаляем слэши
+      $str= trim($str); // удаляем пробелы по бокам
+      $str= htmlspecialchars($str); // переводим HTML в текст
+      $str= preg_replace("~ +~", " ", $str); // множественные пробелы 
+
+      return $str;
+}
+if ($_GET['action'] =='add'){
+	$pname = '';
+	$pemail='';
+	$ptext='';
+	if (isset($_POST['pname'])) {
+		$pname = $_POST['pname'];
+	} else {$pname = '';}
 		
-		if (isset($_POST['pemail'])) {
-			$pemail = $_POST['pemail'];
-		} else {$pemail = '';}
+	if (isset($_POST['pemail'])) {
+		$pemail = $_POST['pemail'];
+	} else {$pemail = '';}
 		
-		if (isset($_POST['ptext'])) {
-			$ptext = $_POST['ptext'];
-		} else {$ptext = '';}
-		
-		if ($ptext =='' && $ptext =='' && $ptext ==''){
-			$res = 'Не введены обязательные поля.';
+	if (isset($_POST['ptext'])) {
+		$ptext = $_POST['ptext'];
+	} else {$ptext = '';}
+	
+	if ($ptext =='' && $ptext =='' && $ptext ==''){
+		$res = 'Не введены обязательные поля.';
+	} else {
+		$pname= StrProcess($pname); // обработка переменной
+		$ptext= StrProcess($ptext); // обработка переменной
+			
+		// посылаем запрос на вставку данных в таблицу
+		$r = mysql_query("insert into reedback (fio, email, text) values ('".$pname."','".$pemail."','".$ptext."')");
+		if ($r == 'true') {
+			$res = $pname.", cпасибо за ваше мнение!";
 		} else {
-			$r = mysql_query("insert into reedback (fio, email, text) values ('".$pname."','".$pemail."','".$ptext."')");
-			if ($r == 'true') {
-				$res = $pname.", cпасибо за ваше мнение!";
-			} else {
-				$res = "Возникла ошибка!".mysql_error();
-			}
+			$res = "Возникла ошибка!".mysql_error();
 		}
-		echo "<script type= 'text/javascript'> 
+	}
+	echo "<script type= 'text/javascript'> 
 				 alert('".$res."');
-				 document.location.href = 'http://localhost:8888/pstu/web2/contact.html'; 
-			 </script>";
-		 exit;
+			 document.location.href = 'http://localhost:8888/pstu/web2/contact.html'; 
+		 </script>";
+	exit;
+}
+if ($_GET['action']=='parser'){
+	run_pars_dom();
+	$result = mysql_query("select * from tovar");
+	if (!$result)
+	{
+		exit(mysql_error());
 	}
-	if ($_GET['action']=='parser'){
-		//run_doom();
-		$result = mysql_query("select * from tovar");
-		if (!$result)
-		{
-			exit(mysql_error());
-		}
-		while ($row = mysql_fetch_assoc($result)) {
-			$rows[] = $row;
-		}
-		$res = json_encode($rows, JSON_UNESCAPED_UNICODE);
+	while ($row = mysql_fetch_assoc($result)) {
+		$rows[] = $row;
 	}
-	echo $res;
+	$res = json_encode($rows, JSON_UNESCAPED_UNICODE);
+}
+
+echo $res;
 ?>
 
